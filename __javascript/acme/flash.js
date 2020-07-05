9b0455c7208528b423cf0abe6664c9068137a992
la-ruby <55279023+la-ruby@users.noreply.github.com>
2020-07-04 22:29:41 -0700

    Create flash.js

---
+++ __javascript/acme/flash.js
@@ -0,0 +1,48 @@
+export function start() {
+  document.addEventListener("turbolinks:load", () => {
+    create()
+    show()
+  })
+}
+
+function show() {
+  $('.toast')
+    .toast({ delay: 2000 })
+    .toast('show')
+}
+
+function create() {
+  $.each( getFlashesInDom(), function(index, value) {
+    $(".acme-toasts .inner").append(
+      template(value)
+    )
+  })
+}
+
+/* private */
+function template(body) {
+  return `
+    <div class="toast" role="status" aria-live="polite" aria-atomic="true" zdata-delay=3000 style="z-index: 1">
+      <div class="toast-header">
+        <!-- an icon can be here -->
+        <strong class="mr-auto">
+          <!-- title here -->
+        </strong>
+        <small class="text-muted">just now</small>
+        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="toast-body">
+        ${body}
+      </div>
+    </div>`
+}
+
+function getFlashesInDom() {
+  return $.map( $(".acme-flashes"), function(val,i) {
+    let html = $.trim( $(val).html() )
+    if ( html.length == 0 ) { return }
+    return html
+  })
+}
