32ab63269c62bb9f81d9fe95e9918051ae859c4f
Sam W <sam@sam-we.com>
2022-02-19 20:24:07 -0800

    Refactoring to a support folder

---
+++ templates/test/support/00_simplecov.rb
@@ -0,0 +1,20 @@
+# OUR STUFF
+# Load and launch SimpleCov at the very top of your test/test_helper.rb
+# (or spec_helper.rb, rails_helper, cucumber env.rb, or whatever your
+# preferred test framework uses)
+require 'simplecov'
+if ENV['COVERAGE']
+  SimpleCov.start 'rails' do
+    command_name 'tests'
+    add_filter 'app/channels'
+    add_filter 'app/controllers/applicatin_controller.rb'
+    add_filter 'app/mailers/application_mailer.rb'
+    add_filter 'app/jobs/application_job.rb'
+    add_filter 'app/helpers/application_helper.rb'
+    add_filter 'app/controllers/application_controller.rb'
+    add_filter 'app/controllers/examples_controller.rb'
+  end
+end
+
+
+# OUR STUFF ENDS
