87ba8db1e47b144391264a94de9256addaec3e89
Sam W <sam@sam-we.com>
2022-02-20 08:57:19 -0800

    Fix for fixtures loading error

--- templates/test/support/vcr.rb
+++ templates/test/support/vcr.rb
@@ -1,7 +1,7 @@
 require 'vcr'
 
 VCR.configure do |config|
-  config.cassette_library_dir = "test/fixtures/vcr_cassettes"
+  config.cassette_library_dir = "test/vcr_cassettes"
   config.hook_into :webmock
 end
 
