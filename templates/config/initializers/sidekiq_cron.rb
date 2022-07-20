53cc4cc70f188e9df4264bd3afa0d6e662876b61
la-ruby <55279023+la-ruby@users.noreply.github.com>
2022-07-20 15:41:21 -0700

    Update and rename sidekiq.rb to sidekiq_cron.rb

--- templates/config/initializers/sidekiq_cron.rb
+++ templates/config/initializers/sidekiq_cron.rb
@@ -1,8 +1,3 @@
-require 'sidekiq/web'
-
-#
-# Scheduling
-#
 require 'sidekiq-cron'
 if ENV['DYNO'] =~ /\Aweb/ # runs only when heroku web dyno context detected
   Sidekiq::Cron::Job.load_from_array([
