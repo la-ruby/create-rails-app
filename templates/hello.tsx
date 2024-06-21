8de44faf4ce2bbaae18e477da9f93a8be4e205f4
la-ruby <sam@sam-we.com>
2024-06-20 22:44:44 -0700

    Activate react only if node exists

--- templates/hello.tsx
+++ templates/hello.tsx
@@ -15,7 +15,11 @@ class Hello extends React.Component<{}, {}> {
 // github.com/reactjs/react-rails/issues/1028#issuecomment-862475500
 //
 document.addEventListener("turbo:load", () => {
-  const root = ReactDOM.createRoot(document.getElementById("hello-react-mount-point"));
+  const element = document.getElementById("hello-react-mount-point");
+  if (!element) {
+    return; // Exit the function early
+  }
+  const root = ReactDOM.createRoot(element);
   root.render(
     <React.StrictMode>
       <Hello />
